ddb5cadb507a3399efec0774e31e9253dd7a93af
marciocamello <marciocamello@outlook.com>
2015-01-06 14:17:04 -0300

    Add config files for environment

---
+++ common/config/main-local.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost:3309;dbname=wozedu_yii2_register',
+            'username' => 'root',
+            'password' => 'camello',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+        /*'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+
+            ],
+        ],*/
+    ],
+];
